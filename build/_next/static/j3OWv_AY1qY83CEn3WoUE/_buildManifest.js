b557bea8a31dcffa341600c0b66c9d912c63abb9
ArunrV3 <arunkumar.r@v3analytics.in>
2024-03-11 12:27:24 +0530

    next changes

---
+++ build/_next/static/j3OWv_AY1qY83CEn3WoUE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f1a300032684d465.css","static/chunks/pages/index-14aba538e07f86b9.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
